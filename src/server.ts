ad736d1529373a43872755a21f2243f6e2729882
GolamMuid <golammuid@gmail.com>
2023-06-09 23:03:55 +0600

    added error handler

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
-import logger from './shared/logger'
+import { logger } from './shared/logger'
 
 async function bootstrap() {
   try {
