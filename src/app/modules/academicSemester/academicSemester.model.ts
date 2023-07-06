5b1cf08a7b8673d143f059c2d9b95f51810223d0
GolamMuid <golammuid@gmail.com>
2023-07-06 22:15:41 +0600

    added Semester route

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,4 +1,6 @@
+import httpStatus from 'http-status';
 import { Schema, model } from 'mongoose';
+import ApiError from '../../../errors/ApiError';
 import {
   academicSemesterCodes,
   academicSemesterMonths,
@@ -41,7 +43,24 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
   }
 );
 
-export const User = model<IAcademicSemester, AcademicSemesterModel>(
+// Handling same year same semester issue
+
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+
+  if (isExist) {
+    throw new ApiError(
+      httpStatus.CONFLICT,
+      'Academic semester already exists!'
+    );
+  }
+  next();
+});
+
+export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
   academicSemesterSchema
 );
--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
+import { academicSemesterController } from './academicSemester.controller';
 import { AcademicSemesterValidation } from './academicSemester.validation';
 
 const router = express.Router();
 
 router.post(
-  '/create-user',
-  validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema)
-  //   UserController.createUser
+  '/create-semester',
+  validateRequest(AcademicSemesterValidation.createAcademicSemesterZodSchema),
+  academicSemesterController.createSemester
 );
 
-export const UserRoutes = router;
+export const AcademicSemesterRoutes = router;
