6637271dd6298b937abcd4dc3185a57c25733be8
GolamMuid <golammuid@gmail.com>
2023-07-09 21:46:57 +0600

    finished all error handling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
-import { UserRoutes } from './app/modules/user/user.route';
+import routes from './app/routes';
 
 const app: Application = express();
 
@@ -15,8 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // * Application routes
 
-app.use('/api/v1/users/', UserRoutes);
-app.use('/api/v1/academic-semesters/', AcademicSemesterRoutes);
+// app.use('/api/v1/users/', UserRoutes);
+// app.use('/api/v1/academic-semesters/', AcademicSemesterRoutes);
+
+app.use('/api/v1/', routes);
 
 app.get('/', async (req: Request, res: Response) => {
   res.send('Working Successfully');
@@ -25,5 +27,21 @@ app.get('/', async (req: Request, res: Response) => {
 // * Global Error Handler
 
 app.use(globalErrorHandler);
+
+// * Handle not found
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'Api not found',
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
