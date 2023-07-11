d63ff30ca203728f7c7889dbf491d42efabe6bab
GolamMuid <golammuid@gmail.com>
2023-07-11 22:27:11 +0600

    added academic semester search and filter

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import cors from 'cors';
-import express, { Application, NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status';
+import express, { Application, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import routes from './app/routes';
+import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
+import { UserRoutes } from './app/modules/user/user.route';
 
 const app: Application = express();
 
@@ -15,10 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // * Application routes
 
-// app.use('/api/v1/users/', UserRoutes);
-// app.use('/api/v1/academic-semesters/', AcademicSemesterRoutes);
+app.use('/api/v1/users/', UserRoutes);
+app.use('/api/v1/academic-semesters/', AcademicSemesterRoutes);
 
-app.use('/api/v1/', routes);
+// app.use('/api/v1/', routes);
 
 app.get('/', async (req: Request, res: Response) => {
   res.send('Working Successfully');
@@ -30,18 +30,18 @@ app.use(globalErrorHandler);
 
 // * Handle not found
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.status(httpStatus.NOT_FOUND).json({
-    success: false,
-    message: 'Not Found',
-    errorMessages: [
-      {
-        path: req.originalUrl,
-        message: 'Api not found',
-      },
-    ],
-  });
-  next();
-});
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   res.status(httpStatus.NOT_FOUND).json({
+//     success: false,
+//     message: 'Not Found',
+//     errorMessages: [
+//       {
+//         path: req.originalUrl,
+//         message: 'Api not found',
+//       },
+//     ],
+//   });
+//   next();
+// });
 
 export default app;
